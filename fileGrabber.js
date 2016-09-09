56044b3c81faab7aa280f0fbf5137da0a6827107
Jad Carson <Jad.Carson@gmail.com>
2016-09-09 12:58:15 -0500

    program now grabs all the picture files from my USB

--- fileGrabber.js
+++ fileGrabber.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser')
-
+const fs = require('file-system')
 const app = express();
 
 
@@ -10,8 +10,24 @@ app.use(bodyParser.urlencoded({extended:false}))
 
 app.get('/', (req,res) => {
     console.log("here now")
-    res.send('OKY DOKY')
+    var names = getFiles('D:/pictures')
+    console.log(names)
+    res.send(names)
 })
+
+function getFiles (dir, files_){
+    files_ = files_ || [];
+    var files = fs.readdirSync(dir);
+    for (var i in files){
+        var name = dir + '/' + files[i];
+        if (fs.statSync(name).isDirectory()){
+            getFiles(name, files_);
+        } else {
+            files_.push(name);
+        }
+    }
+    return files_;
+}
 
 
 app.listen(3000);
